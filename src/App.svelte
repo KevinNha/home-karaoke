302f484e3361a5757d81aa60ed67095b9899b3c0
kevinnha <hyunjinnha@gmail.com>
2023-06-28 13:42:01 -0700

    Change documentation link for easy access for future development

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,15 @@
 <script lang="ts">
   import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
 </script>
 
 <main>
   <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
+    <a href="https://svelte.dev/docs/introduction" target="_blank" rel="noreferrer">
       <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
     </a>
   </div>
-  <h1>Vite + Svelte</h1>
+  <h1>Svelte Documentation</h1>
 
   <div class="card">
     <Counter />
@@ -24,7 +20,7 @@
   </p>
 
   <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
+    Click on Svelte logos to learn more
   </p>
 </main>
 
@@ -34,9 +30,6 @@
     padding: 1.5em;
     will-change: filter;
     transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
   }
   .logo.svelte:hover {
     filter: drop-shadow(0 0 2em #ff3e00aa);
