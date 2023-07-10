c7ca5d81c95e4efedc3a79cbbf4050940b7811d8
kevinnha <hyunjinnha@gmail.com>
2023-07-09 22:37:24 -0700

    Basic setup for Chrome Extension with svelte

---
+++ src/components/AudioController.svelte
@@ -0,0 +1,14 @@
+<script lang=ts>
+
+</script>
+
+<div>
+    is this working?
+</div>
+
+<style>
+    div {
+        width: 500px;
+        height: 500px;
+    }
+</style>
---
+++ src/popup/index.ts
@@ -0,0 +1,11 @@
+import AudioController from '../components/AudioController.svelte'
+
+const target = document.getElementById('app');
+
+async function render() {
+    new AudioController({
+        target
+    });
+}
+  
+document.addEventListener('DOMContentLoaded', render);
